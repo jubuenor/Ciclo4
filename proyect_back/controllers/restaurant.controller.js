b0f78882feb1288680b6f8388b41a0999356412c
Juan Guillermo Ruiz Alvarez <ruizalvarez2819@gmail.com>
2022-11-24 16:19:22 -0500

    se crearon las operaciones del back tanto de categoria y restaurante

---
+++ proyect_back/controllers/restaurant.controller.js
@@ -0,0 +1,76 @@
+const restaurantModel = require("../models/restaurant.model")
+const Restaurant = require("../models/restaurant.model")
+let response = {
+    msg: "",
+    exito: false
+}
+
+exports.create = function (req,res) {
+    let restaurant = new Restaurant({
+        _id: req.body.id, /*segun tengo entendido mongo crea un id*/
+        nombre: req.body.nombre
+    })
+    restaurant.save(function(err){
+        if (err) {
+            console.error(err),
+            response.exito = false,
+            response.msg = "Error al guardar el restaurante"
+            res.json(response)
+            return;
+        }
+
+        response.exito = true,
+        response.msg = "El restaurante se guardo correctamente"
+        res.json(response)
+    })
+}
+
+exports.find = (req,res)=>{
+    Restaurant.find((err,restaurant)=>{
+        res.json(restaurant);
+    });
+}
+
+
+exports.findOne = function(req,res) {
+    Restaurant.findOne({_id: req.params.id}, function(err, restaurant){
+        res.json(restaurant)
+    })
+}
+
+exports.update = function (req,res) {
+    let restaurant = new Restaurant({
+        _id: req.body.id,
+        nombre: req.body.nombre
+    })
+    Restaurant.findByIdAndUpdate(req.params.id,{$set: restaurant},function(err){
+        if (err) {
+            console.error(err),
+            response.exito = false,
+            response.msg = "Error al guardar la categoria"
+            res.json(response)
+            return;
+        }
+
+        response.exito = true,
+        response.msg = "la categoria se guardo correctamente"
+        res.json(response)
+    })
+}
+
+exports.remove = function(req,res){  
+    
+    Restaurant.findByIdAndRemove({_id: req.params.id},function(err){
+        if (err) {
+            console.error(err),
+            response.exito = false,
+            response.msg = "Error al eliminar el restaurante"
+            res.json(response)
+            return;
+        }
+
+        response.exito = true,
+        response.msg = "El restaurante se elimino correctamente"
+        res.json(response)
+    })
+}
---
+++ proyect_back/models/category.model.js
@@ -0,0 +1,10 @@
+const moongose = require("mongoose");// Estamos creando la dependencia de mongo
+const Schema = moongose.Schema;
+
+const CategoryShema = new Schema({
+    _id:{type: Number, required: true},
+    id_restuaurante:{type: String, required:true, max:60},
+    nombre:{type: String, required:true, max:60},
+})
+
+module.exports = moongose.model("Category",CategoryShema)
